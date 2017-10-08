0d80bbe541139d47e9436cc18f0d2ca57bb5cf63
user <2l-u-b-o-v@ukr.net>
2017-10-08 12:31:02 +0300

    исправлено - кнопка заказать

--- resources/views/store/show.blade.php
+++ resources/views/store/show.blade.php
@@ -3,8 +3,12 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div style="display: inline-block;margin-right: 10px"> <a href="/basket/"><img src="../uploads/cart.png" width="20" height="20"> Корзина</a></div>
-        </div>
+            <div style="display: inline-block;margin-right: 10px"><a href="/cart/"><img src="../uploads/cart.png"
+                                                                                        width="20" height="20">
+                    Корзина
+                    <span class="badge">{{Session::has('cart') ? Session::get('cart')->totalQty : ''}}</span>
+
+                </a></div>        </div>
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Имя: {{$good->name}}</div>
@@ -18,11 +22,12 @@
                                 <div>Описание: {{$good->description}}</div>
                                 <div>Категория: {{$cat->cat_name}}</div>
                                 <td>
-                                    <a href="/good/destroy/{{$good->id}}">
+                                    <div class="element"><a href="/add-to-cart/{{$good->id}}">
                                         <button>
                                             Заказать
                                         </button>
                                     </a>
+                                    </div>
 
                                 </td>
                             </tr>
