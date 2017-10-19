525ba6e6a20f6f4ec5d57a691343a5b7551590cf
LDanchenko <2l-u-b-o-v@ukr.net>
2017-10-19 14:19:51 +0300

    js

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,4 @@
-/*function addToCart(id) {
-    //alert(id);
-    //когда добавляешь на бэке нужна проверка!
-    $.ajax({
-        url: "add_to_cart",
-        cache: false,
-        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-        data: {id: id}, // если нужно передать какие-то данные
-        type: "POST", // устанавливаем типа запроса POST
-        success: function (res) {
-            //alert('ok');
-            var qty = document.getElementById('qty');
-            qty.innerHTML = 'd';
-        },
-        error: function (msg) {
-            console.log(msg);
-        }
-    });
 
-}
-*/
-
-//тут через доне
 
 function addToCart(id) {
 
@@ -32,7 +10,7 @@ function addToCart(id) {
             method: 'POST', //отправляем данные методом пост
             cache: false,
             headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-            data: {id: id} // если нужно передать какие-то данные
+            data: {id: id}
         }).done(function (data) {//ответ от формы
 
             var answer = JSON.parse(data);
